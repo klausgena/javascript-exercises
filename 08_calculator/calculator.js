3f6bbab10e0097bf4a26fcf2d19a1e330aaa430b
Nicolas Severyns <n@svrns.com>
2023-08-30 11:36:05 +0100

    Make exercise 08. STILL HAVE TO TEST.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(aNumber) {
+  let sum = 0;
+  for (number of aNumber)	{
+    sum = sum + number;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(aNumber) {
+  let result = 1;
+  for (number of aNumber) {
+    result = result * number;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	// a * b times a
+  let counter = 0;
+  while (counter < b) {
+    a = a * a;
+    counter++;
+  }
+  return a;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  while (a > 0)
+  {
+    a = a * a -1;
+    a--;
+  }	
+  return a;
 };
 
 // Do not edit below this line
@@ -31,3 +50,7 @@ module.exports = {
   power,
   factorial
 };
+
+// debug
+
+add(2,3,4);
