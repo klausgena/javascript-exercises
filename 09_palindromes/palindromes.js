be7e0c76d59b71bb18f867497f1cd909c9fab4f6
Nicolas Severyns <contact@nicolasseveryns.be>
2023-08-30 17:20:09 +0200

    Solve 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const aString = string.split("");
+    // get away with spaces and punctuation
+    let aCleaned = aString.filter(str => (/[a-zA-Z0-9]/).test(str));
+    // make it all lowercase
+    aCleaned = aCleaned.map(str => str.toLowerCase());
+    const sOriginal = aCleaned.join("");
+    const sReversed = (aCleaned.reverse()).join("");
+    if (sOriginal == sReversed) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
