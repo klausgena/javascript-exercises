5ac6c71fc8a2277f668c2facc65ed69875f40d70
Nicolas Severyns <contact@nicolasseveryns.be>
2023-08-30 19:12:26 +0200

    Solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (aPeople) {
+    const aSorted = aPeople.sort(
+        (a, b) => {
+            const aYearOfDeath = !a.yearOfDeath ? new Date().getFullYear() : a.yearOfDeath;
+            const bYearOfDeath = !b.yearOfDeath ? new Date().getFullYear() : b.yearOfDeath;
+            const ageA = aYearOfDeath - a.yearOfBirth;
+            const ageB = bYearOfDeath - b.yearOfBirth;
+            if (ageA > ageB) {
+                return -1;
+            }
+            if (ageA < ageB) {
+                return 1;
+            }
+        }
+    );
+    return aSorted[0];
 };
 
 // Do not edit below this line
